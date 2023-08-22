d5b9789fecac6ccb6fe57546389d4447fa56515a
Arya <arya.patel@artoon.in>
2023-08-22 11:20:35 +0530

    While Penalty User Not Able To Leave

--- src/Config/devConfig.ts
+++ src/Config/devConfig.ts
@@ -36,8 +36,8 @@ const DevConfig = (): ConfigInterface => {
         PLUS_ON_PLUS: true,
 
         GAME_START_TIMER: 1,
-        USER_TURN_TIMER: 50,
-        ROUND_TIMER: 3,
+        USER_TURN_TIMER: 100,
+        ROUND_TIMER: 500,
         NEXT_ROUND_TIMER: 1,
         RETURN_TO_TABLE_TIMER: 30,
         UNO_CLICK_TIMER: 2,
